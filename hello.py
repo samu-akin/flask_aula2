00e18dd94a9fdd6c2d5ff277c502e132713c303a
samu-akin <129891579+samu-akin@users.noreply.github.com>
2024-10-26 12:09:09 -0300

    updates hello.py

--- hello.py
+++ hello.py
@@ -1,50 +1,11 @@
-from datetime import datetime
-from flask import Flask, render_template, request, make_response, redirect, abort
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Chave forte'
 
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
-@app.route('/')
-def index():
-     return render_template('index.html', current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>/<prontuario>/<institution>')
-def user(name, prontuario, institution):
-    return render_template('user.html', 
-                           name=name, 
-                           prontuario=prontuario,
-                           institution=institution);
-
-@app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    user_agent = request.headers.get('User-Agent');
-    remote_addr = request.remote_addr;
-    remote_host = request.host;
-    return render_template('contexto.html',
-                           name=name, 
-                           user_agent=user_agent, 
-                           remote_addr=remote_addr,
-                           remote_host=remote_host);
-
-
-
-
-
-
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [DataRequired()])
+    submit = SubmitField('Submit')
 
