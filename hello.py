18ef9f27c8e68ee7bb96c26346b4cd9787f65e8d
samu-akin <129891579+samu-akin@users.noreply.github.com>
2024-10-26 12:33:05 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask import Flask, render_tamplate, session, redirect, url_for, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Chave forte'
@@ -8,4 +9,14 @@ app.config['SECRET_KEY'] = 'Chave forte'
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [DataRequired()])
     submit = SubmitField('Submit')
-
+    
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'))
